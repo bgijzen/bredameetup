0f2aff591b43affd55124c4abff367fcb04ebf03
Bas Gijzen <Bas.Gijzen@cm.com>
2023-11-11 14:35:54 +0100

    Add Alias any Type

---
+++ Net8Examples/AliasTypes/AliasTypeExample.cs
@@ -0,0 +1,9 @@
+﻿namespace Net8Examples.AliasTypes; 
+
+using Conversation = (string person1, string person2);
+
+public class AliasTypeExample {
+    public static Conversation CreateConversation() {
+        return new("Alice", "Bob");
+    }
+}
---
+++ Net8ExamplesTests/AliasTypeExampleTest.cs
@@ -0,0 +1,15 @@
+﻿using Net8Examples.AliasTypes;
+
+namespace Net8ExamplesTests; 
+
+public class AliasTypeExampleTest {
+    [Fact]
+    public void AliasesOnlyWorkInTheScope() {
+        var conversation = AliasTypeExample.CreateConversation();
+        conversation.person1.Should().Be("Alice");
+        conversation.person2.Should().Be("Bob");
+    }
+    
+    
+    //Show Global Using
+}
